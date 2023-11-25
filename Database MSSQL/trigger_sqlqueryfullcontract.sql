16690bf39c8b93bdbdebd1ccd7151701ab629851
DinhNguyen2004 <118755132+DinhNguyen2004@users.noreply.github.com>
2023-11-25 22:32:10 +0700

    Add files via upload

---
+++ Database MSSQL/trigger_sqlqueryfullcontract.sql
@@ -0,0 +1,63 @@
+﻿/* Trigger FullContract */
+CREATE TRIGGER TG_FULLCONTRACT_INSERT_AUTOCODE
+ON Full_Contract
+FOR INSERT
+AS
+BEGIN	
+	DECLARE @ID INT, @MAVUNG VARCHAR(2), @LOAIHINH VARCHAR(2), @NAMKIKET VARCHAR(2), @THANGKIKET VARCHAR(2), @LOAIHD VARCHAR(2), @MAX INT,@FULL_CONTRACT_CODE NVARCHAR(50), @CUSTOMER_NAME NVARCHAR(100),
+	@YEAR_OF_BIRTH INT, @SSN VARCHAR(15), @CUSTOMER_ADDRESS NVARCHAR(100), @MOBILE VARCHAR(15),@DATE_OF_CONTRACT DATE, @PROPERTY_ID INT,
+	@PRICE NUMERIC(18,0), @DEPOSIT NUMERIC(18,0), @REMAIN NUMERIC(18,0), @STATUS BIT
+
+	
+	SET @ID = (SELECT ID FROM inserted)
+
+	---------------ĐÂY LÀ CODE CỨNG----------------
+	SET @MAVUNG= 'SG' --HN: HÀ NỘI . other: LA: LONG AN, CM: CÀ MAU,... 
+	SET @LOAIHINH = 'CH' --BT: biệt thự, CH: căn hộ, CC: chung cư,...
+	SET @LOAIHD = 'MB' --MB: Mua bán; TD: Thuê đất, NN: Nhà nước,...
+	SET @NAMKIKET = CONVERT(VARCHAR(2),RIGHT(YEAR(GETDATE()),2))
+	SET @THANGKIKET = CONVERT(VARCHAR(2), MONTH(GETDATE()))
+
+	---------------ĐÂY LÀ CODE TỪ NGƯỜI DÙNG NHẬP VÀO.NHƯNG PHẢI TẠO 3 CỘT TẠM THỜI VÀO BẢNG FULLCONTRACT -----------------
+	/* SET @MAVUNG = (SELECT MAVUNG FROM inserted)
+	SET @LOAIHINH = (SELECT LOAIHINH FROM inserted)
+	SET @LOAIHD = (SELECT LOAIHD FROM inserted) */
+
+	IF NOT EXISTS (SELECT 1 FROM Full_Contract WHERE SUBSTRING(Full_Contract_Code, 5, 2)=@NAMKIKET)
+		SET @MAX = 1
+	ELSE
+		SET @MAX = (SELECT MAX(RIGHT(Full_Contract_Code,5)) FROM Full_Contract WHERE SUBSTRING(Full_Contract_Code,5,2)=@NAMKIKET) + 1
+	SET @FULL_CONTRACT_CODE = CONCAT(@MAVUNG,@LOAIHINH,@NAMKIKET,@THANGKIKET,@LOAIHD,RIGHT('0000'+CONVERT(VARCHAR(5), @MAX),5))
+	SET @CUSTOMER_NAME = (SELECT Customer_Name FROM inserted)
+	SET @YEAR_OF_BIRTH = (SELECT Year_Of_Birth FROM inserted)
+	SET @SSN = (SELECT SSN FROM inserted)
+	SET @CUSTOMER_ADDRESS = (SELECT Customer_Address FROM inserted)
+	SET @MOBILE = (SELECT Mobile FROM inserted)
+	SET @PROPERTY_ID = (SELECT Property_ID FROM inserted)
+	SET @DATE_OF_CONTRACT = (SELECT Date_Of_Contract FROM inserted)
+	SET @PRICE = (SELECT Price FROM inserted)
+	SET @DEPOSIT = (SELECT Deposit FROM inserted)
+	SET @REMAIN = (SELECT Remain FROM inserted)
+	SET @STATUS= (SELECT Status FROM inserted) 
+
+	UPDATE Full_Contract SET Full_Contract_Code= @FULL_CONTRACT_CODE WHERE ID=@ID
+
+END
+
+/* Các câu lệnh xóa data có sẵn
+DELETE FROM Full_Contract
+*/
+/* Test insert
+SELECT * FROM  Full_Contract
+
+INSERT INTO Full_Contract(Customer_Name,Year_Of_Birth,SSN,Customer_Address,
+					Mobile,Property_ID , Date_Of_Contract, Price, Deposit, Remain, Status)
+VALUES(N'Nguyễn Ngọc Định',2002,'21554888',N'10 Nguyễn Thị Yến, Ghềnh Ráng, Quy Nhơn, Bình Định','0911466489', 24,
+		'2025-05-30',1234567890,1000000000,6000000000,1)
+
+INSERT INTO Full_Contract(Customer_Name,Year_Of_Birth,SSN,Customer_Address,
+					Mobile, Property_ID, Date_Of_Contract, Price, Deposit, Remain, Status)
+VALUES(N'Nguyễn Ngọc Công',1993,'21444444',N'15 Nguyễn Thị Yến, Ghềnh Ráng, Quy Nhơn, Bình Định','0911467945', 25
+		,'2025-05-28',1234567890,500000000,9000000000,1)
+*/
+
---
+++ Database MSSQL/trigger_sqlqueryproperty.sql
@@ -0,0 +1,59 @@
+﻿/* Trigger Property */
+CREATE TRIGGER TG_PROPERTY_INSERT_AUTOCODE
+ON Property
+FOR INSERT
+AS
+BEGIN
+	DECLARE @ID INT, @MAVUNG VARCHAR(2), @LOAIHINH VARCHAR(2), @NAMHT VARCHAR(2), @THANGHT VARCHAR(2), @MAX INT, @Property_Code VARCHAR(15), @Property_Name NVARCHAR(50), @Property_Type_ID INT, 
+	@Description NVARCHAR, @District_ID INT, @Address NVARCHAR(100), @Area INT, @Bed_Room INT, @Bath_Room INT, @Price NUMERIC(18,0),
+	@Installment_Rate FLOAT, @Avatar NVARCHAR(MAX), @Album NVARCHAR(MAX), @Property_Status_ID INT
+	SET @ID = (SELECT ID FROM inserted)
+	SET @MAVUNG = 'SG' --SG: Saigon . other: VL: Vinh Long, LA: Long An,...
+	SET @LOAIHINH = 'BT' --BT: Biet thu, CH: Can Ho, CC: Chung cu,...
+	SET @NAMHT = CONVERT(VARCHAR(2),RIGHT(YEAR(GETDATE()),2))
+	SET @THANGHT = CONVERT(VARCHAR(2), MONTH(GETDATE()))
+	IF NOT EXISTS (SELECT 1 FROM Property WHERE SUBSTRING(Property_Code,3,2)=@NAMHT)
+		SET @MAX = 1
+	ELSE
+		SET @MAX = (SELECT MAX(RIGHT(Property_Code,5)) FROM Property WHERE SUBSTRING(Property_Code,3,2)=@NAMHT) + 1
+	SET @Property_Code = @MAVUNG+@NAMHT+@THANGHT+@LOAIHINH+RIGHT('0000'+CONVERT(VARCHAR(5), @MAX),5)
+	SET @Property_Name = (SELECT Property_Name FROM inserted)
+	SET @Property_Type_ID = (SELECT Property_Type_ID FROM inserted)
+	SET @Description = (SELECT Description FROM inserted)
+	SET @District_ID = (SELECT District_ID FROM inserted)
+	SET @Address = (SELECT Address FROM inserted)
+	SET @Area = (SELECT Area FROM inserted)
+	SET @Bed_Room = (SELECT Bed_Room FROM inserted)
+	SET @Bath_Room = (SELECT Bath_Room FROM inserted)
+	SET @Price = (SELECT Price FROM inserted)
+	SET @Installment_Rate = (SELECT Installment_Rate FROM inserted)
+	SET @Avatar= (SELECT Avatar FROM inserted)
+	SET @Album = (SELECT Album FROM inserted)
+	SET @Property_Status_ID = (SELECT Property_Status_ID FROM inserted)
+
+	UPDATE Property SET Property_Code=@Property_Code WHERE ID=@ID
+END
+
+/* Các câu lệnh xóa data có sẵn
+DELETE FROM Property; Xóa property trước khi test
+*/
+
+/* Test insert
+SELECT * FROM PROPERTY
+
+INSERT INTO Property(Property_Name,Property_Type_ID,Description,District_ID,
+					Address, Area, Bed_Room, Bath_Room, Price, Installment_Rate, Avatar, Album,Property_Status_ID)
+VALUES(N'BIỆT THỰ PHÚ MỸ HƯNG',4,N'Không khí trong lành',3,N'Khu Nam Viên, Phú Mỹ Hưng, Quận 7, TP.HCM',
+		1000,5,5,1234567890,7.99,'phumyhung.jpg','listpmh.jpg',5)
+
+INSERT INTO Property(Property_Name,Property_Type_ID,Description,District_ID,
+					Address, Area, Bed_Room, Bath_Room, Price, Installment_Rate, Avatar, Album,Property_Status_ID)
+VALUES(N'CHƯNG CƯ VINHOMES',2,N'Cao tầng đẹp đẽ',3,N'Quận 9, TP.HCM',
+		1000,5,5,1234567890,7.99,'vinhomes.jpg','listvinhomes.jpg',5)
+*/
+
+
+
+
+
+
